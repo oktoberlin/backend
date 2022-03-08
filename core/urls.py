6a01368a09955777706128b68ce2d6aa0e281784
oktoberlin <oktoberlin@gmail.com>
2022-03-08 15:58:37 +0700

    finish authentication

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
-    #path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
+    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
 ] + static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
 
